ba7faa382b7ba83cce0bc461fd180d7226ef7366
gizemyuksel <gizem.682@windowslive.com>
2024-10-20 18:49:25 +0300

    Initial Commit

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Gizem Yüksel on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
